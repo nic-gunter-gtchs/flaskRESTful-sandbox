f150476de0ea6707fbab3a16775a898928bd0b15
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-17 12:10:22 -0400

    Update fileman.py

--- src/fmLib/fileman.py
+++ src/fmLib/fileman.py
@@ -6,3 +6,18 @@ def newF(name, directory="../", extension="txt"):
   directory: The directory to hold the new file. Default is the directory the library resides in. If the directory does not exist, it will be created.
   extension: The file extension the file will use. Default is txt.
   """
+
+def readF(path, write=False):
+  """
+  Opens an existing file and returns its contents.
+  Parameters:
+  path: The path to the file. This argument must be passed.
+  write: If True, sets the file up for writing. This will still return the contents of the file.
+  """
+
+def readS(path, type="json"):
+  """
+  Reads specially formatted files.
+  Parameters:
+  path: The path to the file. This argument must be passed.
+  
