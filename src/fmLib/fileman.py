dd3122bfc774d7974a15ff2c48ef5128819a86f3
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-17 12:03:09 -0400

    Create fileman.py

---
+++ src/fmLib/fileman.py
@@ -0,0 +1,8 @@
+def newF(name, directory="../", extension="txt"):
+  """
+  Creates a new file and returns its file path.
+  Parameters:
+  name: The new file's name. This argument must be passed.
+  directory: The directory to hold the new file. Default is the directory the library resides in. If the directory does not exist, it will be created.
+  extension: The file extension the file will use. Default is txt.
+  """
