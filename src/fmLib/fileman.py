d133f2953943b25a9fb2d281d83133f219c8288a
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-17 12:20:07 -0400

    Update fileman.py

--- src/fmLib/fileman.py
+++ src/fmLib/fileman.py
@@ -1,3 +1,5 @@
+import json
+import xml.etree.ElementTree as ET
 def newF(name, directory="../", extension="txt"):
   """
   Creates a new file and returns its file path.
@@ -6,7 +8,15 @@ def newF(name, directory="../", extension="txt"):
   directory: The directory to hold the new file. Default is the directory the library resides in. If the directory does not exist, it will be created.
   extension: The file extension the file will use. Default is txt.
   """
-
+  try:
+    f = open(f"{directory}{name}.{extension}", "x") # makes the new file
+  except Exception as exception:
+    # if some error happens, handle it
+    print(f"File opening failed. Exception: \n{exception}")
+  finally:
+    path = directory+name+"."+extension
+    return path
+  return
 def readF(path, write=False):
   """
   Opens an existing file and returns its contents.
