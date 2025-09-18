63b72e94572bd9d4fdcdc1dc6aa3b5e5d2b65494
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-18 14:36:17 -0400

    Update main.py

--- src/apiTest/main.py
+++ src/apiTest/main.py
@@ -1,5 +1,20 @@
 from fmLib import fileman as FM
-from flask import Flask
-from flask_restx import Api, Resource, reqparse
+from flask import Flask, request
+from flask_restx import Api, Resource
+from markupsafe import escape
+
 app = Flask(__name__)
 api = Api(app)
+
+@api.route("/tests/fm/api")
+class FileMan(Resource):
+  def put(self):
+    type = request.form["data"]["type"]
+    match type:
+      case "create":
+        dir = request.form["data"]["directory"]
+        fname = request.form["data"]["name"]
+        
+        pass
+      case "update":
+        
